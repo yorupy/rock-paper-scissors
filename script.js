b94fc071a23b1ab3453640981ac75c120f42214f
yorupy <198967142+yorupy@users.noreply.github.com>
2025-02-22 11:25:32 -0500

    Play singular rounds

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) return "rock";
@@ -18,4 +21,32 @@ function capitalize(text) {
     return text[0].toUpperCase() + text.slice(1);
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "It is a draw!";
+    } else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            return generateRoundString("Computer", computerChoice, humanChoice);
+        } else {
+            return generateRoundString("Human", humanChoice, computerChoice);
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            return generateRoundString("Computer", computerChoice, humanChoice);
+        } else {
+            return generateRoundString("Human", humanChoice, computerChoice);
+        }
+    } else {
+        if (computerChoice === "rock") {
+            return generateRoundString("Computer", computerChoice, humanChoice);
+        } else {
+            return generateRoundString("Human", humanChoice, computerChoice);
+        }
+    }
+}
+
+function generateRoundString(winner, winnerChoice, loserChoice) {
+    return `${winner} wins! ${capitalize(winnerChoice)} beats ${capitalize(loserChoice)}`;
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));

b9c6c4bf744c5a1ac559679a101b55fc15ea1ccc
yorupy <198967142+yorupy@users.noreply.github.com>
2025-02-22 11:34:17 -0500

    Add game loop

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
+
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
@@ -21,32 +20,49 @@ function capitalize(text) {
     return text[0].toUpperCase() + text.slice(1);
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        return "It is a draw!";
-    } else if (humanChoice === "rock") {
-        if (computerChoice === "paper") {
-            return generateRoundString("Computer", computerChoice, humanChoice);
-        } else {
-            return generateRoundString("Human", humanChoice, computerChoice);
-        }
-    } else if (humanChoice === "paper") {
-        if (computerChoice === "scissors") {
-            return generateRoundString("Computer", computerChoice, humanChoice);
-        } else {
-            return generateRoundString("Human", humanChoice, computerChoice);
-        }
-    } else {
-        if (computerChoice === "rock") {
-            return generateRoundString("Computer", computerChoice, humanChoice);
-        } else {
-            return generateRoundString("Human", humanChoice, computerChoice);
-        }
-    }
-}
+
 
 function generateRoundString(winner, winnerChoice, loserChoice) {
     return `${winner} wins! ${capitalize(winnerChoice)} beats ${capitalize(loserChoice)}`;
 }
 
-console.log(playRound(getHumanChoice(), getComputerChoice()));
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            return "It is a draw!";
+        } else if (humanChoice === "rock") {
+            if (computerChoice === "paper") {
+                return generateRoundString("Computer", computerChoice, humanChoice);
+            } else {
+                return generateRoundString("Human", humanChoice, computerChoice);
+            }
+        } else if (humanChoice === "paper") {
+            if (computerChoice === "scissors") {
+                return generateRoundString("Computer", computerChoice, humanChoice);
+            } else {
+                return generateRoundString("Human", humanChoice, computerChoice);
+            }
+        } else {
+            if (computerChoice === "rock") {
+                return generateRoundString("Computer", computerChoice, humanChoice);
+            } else {
+                return generateRoundString("Human", humanChoice, computerChoice);
+            }
+        }
+    }
+    while (humanScore !== 5 && computerScore !== 5) {
+        const result = playRound(getHumanChoice(), getComputerChoice());
+        if (result.startsWith("Human")) humanScore++;
+        else if (result.startsWith("Computer")) computerScore++;
+        console.log(result);
+    }
+    if (humanScore === 5) {
+        console.log("Human wins the game! Computers suck at RPS!");
+    } else {
+        console.log("Computer wins the game! Machines will rule the world!");
+    }
+}
+
+playGame();
